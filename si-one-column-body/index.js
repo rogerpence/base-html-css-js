5060b6183ab8dc25c357e1b4e4a2096e1eda3ffe
Roger Pence <roger.pence@gmail.com>
2019-05-28 21:39:34 -0500

    Initial commit

---
+++ si-one-column-body/index.js
@@ -0,0 +1,45 @@
+const assignOneTimeEventHandler = function (target, eventName, fn, delay = 0) {
+    // A delay of 250ms or so may be necessary when working with dynamically-added
+    // DOM elements.
+    const eventHandler = (event) => {
+        if (fn && typeof fn === 'function') {
+            setTimeout(() => {
+                fn(target);
+            }, delay)
+        }
+        target.removeEventListener(eventName, eventHandler);
+    }
+
+    target.addEventListener(eventName, eventHandler);
+}
+
+const actions = {
+    search: function(e) {
+        const navbar = document.getElementById('secondary-nav');        
+        navbar.classList.toggle('expanded');
+        e.target.classList.toggle('rotate180')
+
+        if (navbar.classList.contains('expanded')) {
+            document.getElementById('customer-search-value').focus();
+        }            
+
+        assignOneTimeEventHandler(navbar, 'transitionend', (elem) => {
+            //navbar.classList.toggle('padding');
+        }, 10)
+    }
+}
+
+camelize = function camelize(str) {
+    return str.replace(/\W+(.)/g, function (match, chr) {
+        return chr.toUpperCase();
+    });
+}
+
+const actionButtons = Array.from(document.querySelectorAll('.action-button'));
+actionButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const methodName = camelize(button.id);
+        actions[methodName](e);
+    });
+
+})
